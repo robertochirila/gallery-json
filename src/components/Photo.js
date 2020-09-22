aed0684985fafb88c9cfe34bdc4179b7703ca4ba
Roberto <robertochirila@yahoo.com>
2020-09-22 19:45:05 +0100

    added mobile first design using css grid

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,24 +2,48 @@ import React from "react";
 import styled from "styled-components";
 
 const PhotoGallery = styled.img`
-     width: 60%;
+     width: 90%;
+     margin-left: 5%;
      height: auto;
-     margin: 20%;
+     @media (min-width: 600px) {
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+     }
+     @media (min-width: 1000px) {
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+     }
 `;
 
 const ContainerDescription = styled.div`
-     width: 60%;
+     width: 90%;
      height: auto;
-     margin: 20%;
      background: rgb(0, 122, 204);
+     margin-left: 5%;
+     @media (min-width: 600px) {
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+     }
+     @media (min-width: 1000px) {
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+     }
 `;
 
 const PhotoDescription = styled.h3`
      color: #fff;
+     text-align: center;
+     padding: 5%;
      margin: 10%;
+     @media (min-width: 600px) {
+          padding: 2%;
+          margin: 5%;
+     }
+     @media (min-width: 1000px) {
+          padding: 0;
+          margin: 2%;
+     }
 `;
 
-const PhotoWrapper = styled.div``;
+const PhotoWrapper = styled.div`
+     position: relative;
+`;
 
 const Photo = (props) => {
      const { photo } = props;
